ffb75ab9aaaf959e6efbc983819b21e4d25a00c6
超 李 <lichao_bj@hotmail.com>
2016-06-02 00:11:03 +0800

    添加项目文件。

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Client.CalculatorServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CaculatorServiceClient proxy = new CaculatorServiceClient())
+            {
+                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Substract(1, 2));
+                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Services/CalculatorService.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CalculatorService : ICalculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x/y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x -y;
+        }
+    }
+}
